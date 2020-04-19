cfa0dbfc0cea117f93d93d7ceeed044468e41d44
eifory <bartem007@gmail.com>
2020-04-19 08:26:26 +0300

    fix test

--- nodejs-hw2/__tests__/getDirTree.test.js
+++ nodejs-hw2/__tests__/getDirTree.test.js
@@ -3,9 +3,10 @@ const path = require('path');
 const getDirTree = require('../getDirTree');
 
 const fixturePath = path.join(__dirname, '..', '/__fixtures__/');
+const testDepth = 3;
 
 test('getDirTree', () => {
   const expected = fs.readFileSync(path.join(fixturePath, 'result'), 'utf8');
-  expect(getDirTree(fixturePath, 3)).toBe(expected);
+  expect(getDirTree(fixturePath, testDepth)).toBe(expected);
 })
 
