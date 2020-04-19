e78b5228a58e8cca6dfbe785841139bfe82612bd
eifory <bartem007@gmail.com>
2020-04-19 10:45:33 +0300

    add new test

--- nodejs-hw2/__tests__/getDirTree.test.js
+++ nodejs-hw2/__tests__/getDirTree.test.js
@@ -3,10 +3,15 @@ const path = require('path');
 const getDirTree = require('../getDirTree');
 
 const fixturePath = path.join(__dirname, '..', '/__fixtures__/');
-const testDepth = 3;
+const testDepth = 2;
 
-test('getDirTree', () => {
-  const expected = fs.readFileSync(path.join(fixturePath, 'result'), 'utf8');
+test('getDirTree without argument depth', () => {
+  const expected = fs.readFileSync(path.join(fixturePath, 'result-without-arg'), 'utf8');
+  expect(getDirTree(fixturePath)).toBe(expected);
+})
+
+test('getDirTree with argument depth = 2', () => {
+  const expected = fs.readFileSync(path.join(fixturePath, 'result-with-arg-2'), 'utf8');
   expect(getDirTree(fixturePath, testDepth)).toBe(expected);
 })
 
